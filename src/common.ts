f01f603bc9fbbec65d605d7becb05b733a2de522
Yuriy Plotnikov <yuriy.plotnikov@outlook.com>
2018-09-21 13:50:08 +0300

    Fixed module import

--- src/common.ts
+++ src/common.ts
@@ -5,7 +5,7 @@ import { registerDecorator } from "class-validator";
 import { isString, promisify, isNumber } from "util";
 import axios from "axios";
 import fs from "fs";
-import appInsights from "applicationinsights";
+import * as appInsights from "applicationinsights";
 
 const pkg = require("../package.json");
 const uuidRegExp = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
